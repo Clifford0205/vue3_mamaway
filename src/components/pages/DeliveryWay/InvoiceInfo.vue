ffae70ed57dcda43c25d47a3357deef9bbf52761
田仲展 <ts02690140@gmail.com>
2020-01-03 00:18:47 +0800

    01030018

--- src/components/pages/DeliveryWay/InvoiceInfo.vue
+++ src/components/pages/DeliveryWay/InvoiceInfo.vue
@@ -41,31 +41,50 @@
       </div>
 
       <div class="pickup-method">
-        <p class="text little-title">取貨方式</p>
+        <p class="text little-title">中獎發票寄送</p>
         <div class="options">
           <div class="single-option">
             <input type="radio" name="location" value="Taipei" />
-            <p class="text">宅配到府</p>
+            <p class="text">同收件人</p>
           </div>
 
           <div class="single-option">
             <input type="radio" name="location" value="Taoyuan" />
             <div class="text">
-              <p>超商取貨(線上付款)</p>
-              <p class="red-text">若總體積過大，將改為宅配送貨</p>
-            </div>
-          </div>
-
-          <div class="single-option">
-            <input type="radio" name="location" value="Taoyuan" />
-            <div class="text">
-              <p>超商取貨(取貨付款)</p>
-              <p class="red-text">若總體積過大，將改為宅配送貨</p>
+              <p>其他地址</p>
             </div>
           </div>
         </div>
       </div>
 
+      <div class="grey-area">
+        <div class="choose-invoive-type">
+          <div class="invoive-type">
+            <p class="text little-title">使用載具</p>
+          </div>
+
+          <div class="select-part">
+            <select name="invoice" id class="invoice-select">
+              <option value selected>雲端發票</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="mail-and-phone">
+          <div class="mail single-item">
+            <div class="little-title">載具條碼</div>
+            <input type="text" placeholder="請輸入載具條碼" class="email-input text-input" />
+          </div>
+
+          <div class="phone single-item">
+            <div class="little-title">確認載具條碼</div>
+            <input type="text" placeholder="請再次輸入載具條碼" class="phone-input text-input" />
+          </div>
+        </div>
+      </div>
+
+      <input type="checkbox" name="vehicle1" value="add" class="common-address" />
+      雲端發票服務「雲端發票實施作業要點」我同意辦理退貨時，由媽媽餵Mamaway代為處理發票及銷貨退回證明書，已加速退貨退款作業。
       <!-- Modal -->
       <div
         class="modal fade"
