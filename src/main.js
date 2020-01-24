c49c1b83d2d67e543f8b26782799d09071409191
田仲展 <ts02690140@gmail.com>
2020-01-24 14:16:05 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
+import Vuex from 'vuex'
 import App from './App.vue';
 import axios from 'axios'; //主要ＡＪＡＸ套件
 import VueAxios from 'vue-axios';//將它轉為ＶＵＥ的套件
 import 'bootstrap';
 import router from './router/index';
-
+import store from './store';
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
+Vue.use(Vuex)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
